6fdb55938a3690af0b5b8d18bec48ea58ce6f285
codex-hue <2229132@kiit.ac.in>
2025-07-17 22:50:48 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Simple PHP Project</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome to My Simple PHP Project</h1>
+        <p>Current date and time: <?php echo date('Y-m-d H:i:s'); ?></p>
+    </div>
+</body>
+</html>
